49705785da062a7680b7972608ed6e27115aa906
cmsalazar5 <cmsalaza5@gmail.com>
2013-08-31 02:11:49 -0500

    se agregaron tres menus y las librerias de orm lite

--- UnadNotas/src/com/cristhian/unadnotas/MainActivity.java
+++ UnadNotas/src/com/cristhian/unadnotas/MainActivity.java
@@ -1,15 +1,32 @@
 package com.cristhian.unadnotas;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cristhian.unadnotas.adapter.ImageOptionAdapter;
+import com.cristhian.unadnotas.utils.ItemOpcionMenu;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ClipData.Item;
 import android.view.Menu;
+import android.widget.GridView;
 
 public class MainActivity extends Activity {
-
+	
+	private GridView gvOpciones;
+	private List<ItemOpcionMenu> listaItems = new ArrayList<ItemOpcionMenu>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		// Agrego los items del menu
+		listaItems.add(new ItemOpcionMenu(getResources().getString(R.string.itemCarrera), R.drawable.carrera));
+		listaItems.add(new ItemOpcionMenu(getResources().getString(R.string.itemConvenios), R.drawable.convenios));
+		listaItems.add(new ItemOpcionMenu(getResources().getString(R.string.itemPlanEstudio), R.drawable.plan_estudio));
+		gvOpciones = (GridView)findViewById(R.id.gvOpciones);
+		gvOpciones.setAdapter(new ImageOptionAdapter(this, listaItems));
 	}
 
 	@Override
